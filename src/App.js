511f670a85afac4e9e145a86d0b43839c890aa25
othman19t <othman19t@gmail.com>
2020-06-02 20:18:46 -0700

    setting up routing and deffrent pages with 404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom"; // npm i react-router-dom
 
+const ExpenseDashBoard = () => <div>this is Dashboard component</div>;
+const Create = () => <div>this is create page</div>;
+const Edit = () => <div>this is the Edit page</div>;
+const Help = () => <div>this is the help page</div>;
+const Err404 = () => <div>404 page is not found</div>;
 function App() {
   return (
-    <div className="App">
-      <h1>hello world from App</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ExpenseDashBoard} exact={true} />
+        <Route path="/create" component={Create} exact={true} />
+        <Route path="/edit" component={Edit} exact={true} />
+        <Route path="/help" component={Help} exact={true} />
+        <Route component={Err404} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
